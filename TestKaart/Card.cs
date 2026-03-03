082abba147c40b219a0f953b15a5a89796d376d8
elyh86 <9025500@student.zadkine.nl>
2026-03-03 13:12:18 +0100

    Add project files.

---
+++ TestKaart/Card.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balatrogame
+{
+    class Card
+    {
+        public CardValue Value;
+        public Suit Suit;
+        public Card(CardValue value, Suit suit)
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
+        public void PrintMe()
+        {
+            Console.WriteLine(this.Value);
+        }
+        public string MakeAsString()
+        {
+            return Value.ToString();
+        }
+
+    }
+}
---
+++ TestKaart/Program.cs
@@ -0,0 +1,37 @@
+﻿using balatrogame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balatrogame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Card> deck = new List<Card>();
+
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))  //dit wordt 4 keer uitgvoerd
+            {
+                foreach (CardValue value in Enum.GetValues(typeof(CardValue))) //per keer wordt dit 13 keer uitgevoerd
+                {
+                    Card card = new Card(value, suit);
+                    card.Suit = suit;
+                    card.Value = value;
+                    deck.Add(card);
+                }
+            }
+
+            foreach (Card card in deck)
+            {
+                Console.Write(card.Suit.ToString());
+                Console.WriteLine(card.Value.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
